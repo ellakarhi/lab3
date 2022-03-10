1e556ca22789681d6eb06ade95edb0c5325596eb
Ella Karhi <100384206+ellakarhi@users.noreply.github.com>
2022-03-10 22:18:33 +0000

    completed part 2

---
+++ part1.py
@@ -0,0 +1,13 @@
+import math 
+
+def print_division_info(num1, num2):
+  #x divided by y is n remainder m
+  div = math.floor(num1/num2)
+  rem = math.remainder(num1, num2)
+  print (str(num1) + " divided by " +   str(num2) + " is " + str(div) +      " remainder " + rem) 
+
+  #attempting to print the value of  the remainder variable outside of   body of the function causes an     error because the computer reads the fuction and won't take into account the remainder variable because it is global and not locally within the function.
+  #This causes a runtime error.
+
+print_division_info(4, 3)
+
